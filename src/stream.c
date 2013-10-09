625bc3f324c2b58c6ed481ba39342073465b6b6f
Jon Morehouse <morehousej09@gmail.com>
2013-10-09 00:28:03 -0700

    Update application structure. Change streams to stream and begin building out further structure of components etc

--- src/stream.c
+++ src/stream.c
@@ -1,1 +1,12 @@
 #include "stream.h"
+
+static AVStream * createStream(AVFormatContext * context, AVCodec ** codec, enum AVCodecID codecId) {
+
+	
+}
+
+stream_namespace const stream = {
+
+	.createStream = createStream,
+
+};
